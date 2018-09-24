819bf6e0708e9520b02a9a7964c4d236717a549f
yuanlicc <yl123456789lxp@163.com>
2018-09-24 21:11:43 +0800

    add

---
+++ nio/src/test/java/com/yl/learn/nio/ChannelTestTest.java
@@ -0,0 +1,23 @@
+package com.yl.learn.nio;
+
+import com.yl.learn.nio.buffer.BufferPrinter;
+import com.yl.learn.nio.file.FileReader;
+import org.junit.Test;
+
+public class ChannelTestTest extends TestSuper {
+
+    String relativePath = "test.txt";
+
+    @Test
+    public void testReadFileChar() {
+
+        BufferPrinter.println(FileReader.byteBuffer(relativePath), "", "");
+    }
+
+
+    @Test
+    public void testReadFileByte() {
+        BufferPrinter.println(FileReader.charBuffer(relativePath, "utf-8")
+                , "utf-8", "", "");
+    }
+}
