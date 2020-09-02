d60444d0a3ab006c5275a4f6ea2021cf7b0b20a4
YuanLicc <yl123456789lxp@163.com>
2020-09-02 21:57:45 +0800

    Insert Sort

--- util/src/main/java/com/yl/learn/util/util/SortUtil.java
+++ util/src/main/java/com/yl/learn/util/util/SortUtil.java
@@ -5,13 +5,10 @@ import junit.framework.TestCase;
 public class SortUtil extends TestCase {
     
     public static <T extends Comparable<T>> void selectSort(T[] array) {
-        
         if(array == null || array.length <= 1) return;
         
         int index = 0;
-        
         while(index < array.length) {
-            
             T min = array[index];
             int minIndex = index;
             for(int i = index + 1; i < array.length; i++) {
@@ -20,12 +17,31 @@ public class SortUtil extends TestCase {
                     min = array[i];
                 }
             }
-            
             array[minIndex] = array[index];
             array[index] = min;
-            
             index++;
         }
+    }
+
+    public static <T extends Comparable<T>> void insertSort(T[] array) {
+        if(array == null || array.length <= 1) return;
+
+        for(int i = 1; i < array.length; i++) {
+            for(int j = i - 1; j >= 0; j--) {
+                T cur = array[j];
+                if(array[j + 1].compareTo(cur) < 0) {
+                    T tmp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = tmp;
+                }
+            }
+        }
+    }
+
+    public void testInsertSort() {
+        Integer[] arr = new Integer[]{1, -1, 20, 10, 11, 33};
+        insertSort(arr);
+        PrintUtil.printlnArray(arr);
     }
     
     public void testSelectSort() {
