560215f3015f746fd7b27f263c9e527804af9829
YuanLicc <yl123456789lxp@163.com>
2020-09-02 23:59:31 +0800

    xx

--- util/src/main/java/com/yl/learn/util/util/SortUtil.java
+++ util/src/main/java/com/yl/learn/util/util/SortUtil.java
@@ -38,6 +38,63 @@ public class SortUtil extends TestCase {
         }
     }
 
+    public static <T extends Comparable<T>> void bubbleSort(T[] array) {
+        if(array == null || array.length <= 1) return;
+
+        for(int i = 0; i < array.length; i++) {
+            for(int j = i + 1; j < array.length; j++) {
+                if(array[j].compareTo(array[j - 1]) < 0) {
+                    T tmp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = tmp;
+                }
+            }
+        }
+    }
+
+    public static <T extends Comparable<T>> void mergeSort(T[] array) {
+        if(array == null || array.length <= 1) return;
+        mergeSort(array, 0, array.length - 1);
+    }
+
+    private static <T extends Comparable<T>> void mergeSort(T[] array, int start, int end) {
+        int minus = end - start;
+        if(minus > 1) {
+            int middle = (start + end) >> 1;
+            mergeSort(array, start, middle);
+            mergeSort(array, middle + 1, end);
+
+            merge(array, start, middle, end);
+        }
+
+        if(minus == 1 && array[start].compareTo(array[end]) > 0) {
+            T tmp = array[start];
+            array[start] = array[end];
+            array[end] = tmp;
+        }
+
+    }
+
+    private static <T extends Comparable<T>> void merge(T[] array, int leftStart, int leftEnd, int rightEnd) {
+        int leftIndex = leftStart;
+        int rightIndex = leftEnd + 1;
+
+
+        while (leftIndex <= leftEnd && rightIndex <= rightEnd) {
+
+            if(array[leftIndex].compareTo(array[rightIndex]) > 0) {
+
+            }
+
+        }
+    }
+
+    public void testBubbleSort() {
+        Integer[] arr = new Integer[]{1, -1, 20, 10, 11, 33};
+        bubbleSort(arr);
+        PrintUtil.printlnArray(arr);
+    }
+
     public void testInsertSort() {
         Integer[] arr = new Integer[]{1, -1, 20, 10, 11, 33};
         insertSort(arr);
