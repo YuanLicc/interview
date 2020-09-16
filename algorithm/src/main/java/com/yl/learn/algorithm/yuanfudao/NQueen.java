315a98789df37ba54e3c25617ccc89d383991f3a
YuanLicc <yl123456789lxp@163.com>
2020-09-16 22:20:58 +0800

    N 皇后回溯法

--- algorithm/src/main/java/com/yl/learn/algorithm/yuanfudao/NQueen.java
+++ algorithm/src/main/java/com/yl/learn/algorithm/yuanfudao/NQueen.java
@@ -1,54 +1,104 @@
 package com.yl.learn.algorithm.yuanfudao;
 
+import junit.framework.TestCase;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class NQueen {
-    
+public class NQueen extends TestCase {
+
     public List<List<String>> solveNQueens(int n) {
-        if(n <= 2) return null;
-        
+
         int[][] cell = new int[n][n];
         
         List<List<String>> rs = new ArrayList<>();
-        
+
         dfs(cell, rs, 1, n);
         
         return rs;
+    }
+
+    private void init(int n, List<String> fangAn) {
+
     }
     
     private void dfs(int[][] cell, List<List<String>> rs, int queen, int n) {
         
         if(queen > n) {
-            //
+            rs.add(cellToString(cell));
+            return;
         }
-        
+
+        // 一层一层的放置
         for(int i = 0; i < n; i++) {
             if(queen == 1) {
-                cell[queen - 1][i] = 1;
+                int[] newLine = new int[n];
+                newLine[i] = 1;
+                cell[queen - 1] = newLine;
                 dfs(cell, rs, queen + 1, n);
             }
             else {
                 if(check(cell, queen - 1, i)) {
-                    cell[queen - 1][i] = 1;
-                    dfs(cell, rs, queen + 1, n);
+                    int[][] cp = copy(cell, queen - 2);
+                    cp[queen - 1][i] = 1;
+                    dfs(cp, rs, queen + 1, n);
                 }
             }
         }
     }
+
+    private int[][] copy(int[][] cell, int level) {
+        int[][] cp = new int[cell.length][cell[0].length];
+
+        for(int i = 0; i <= level; i++) {
+            for(int j = 0; j < cell[i].length; j++) {
+                cp[i][j] = cell[i][j];
+            }
+        }
+        return cp;
+    }
+
+    private List<String> cellToString(int[][] cell) {
+        List<String> rs = new ArrayList<>(cell.length);
+
+        for(int i = 0; i < cell.length; i++) {
+            StringBuilder sb = new StringBuilder(cell[i].length);
+            for(int j = 0; j < cell[i].length; j++) {
+                sb.append(cell[i][j] == 0 ? '.' : 'Q');
+            }
+            rs.add(sb.toString());
+        }
+        return rs;
+    }
     
     private boolean check(int[][] cell, int i, int j) {
-        boolean is = false;
-        if(i > 0 && j > 0) {
-            is = cell[i - 1][j - 1] != 1;
+        int i1 = i;
+        int j1 = j;
+        while(i1 >= 0 && j1 >= 0) {
+            if(cell[i1][j1] == 1) return false;
+            i1--;
+            j1--;
         }
-        if(i < cell.length - 1 && j > 0) {
-            is = is && cell[i + 1][j - 1] != 1;
+
+        i1 = i;
+        j1 = j;
+        while(i1 >= 0) {
+            if(cell[i1][j1] == 1) return false;
+            i1--;
         }
-        if(j > 0) {
-            is = is && cell[i][j - 1] != 1;
+
+        i1 = i;
+        j1 = j;
+        while(i1 >= 0 && j1 < cell[i1].length) {
+            if(cell[i1][j1] == 1) return false;
+            i1--;
+            j1++;
         }
         
-        return is;
+        return true;
+    }
+
+    public void test() {
+        solveNQueens(4);
     }
 }
