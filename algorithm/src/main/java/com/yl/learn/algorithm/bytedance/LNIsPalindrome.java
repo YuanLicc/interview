0985fc4422e2ede6caa60b1ff99180913df0d775
YuanLicc <yl123456789lxp@163.com>
2020-09-27 22:19:08 +0800

    leetcode 234 dfs 解法

---
+++ algorithm/src/main/java/com/yl/learn/algorithm/bytedance/LNIsPalindrome.java
@@ -0,0 +1,38 @@
+package com.yl.learn.algorithm.bytedance;
+
+import com.yl.learn.algorithm.ListNode;
+
+/**
+ * 请判断一个链表是否为回文链表。
+ *
+ * 示例 1:
+ *
+ * 输入: 1->2
+ * 输出: false
+ * 示例 2:
+ *
+ * 输入: 1->2->2->1
+ * 输出: true
+ * 进阶：
+ * 你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+ *
+ * 来源：力扣（LeetCode）234
+ * 链接：https://leetcode-cn.com/problems/palindrome-linked-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LNIsPalindrome {
+
+    public boolean isPalindrome(ListNode head) {
+        h = head;
+        return dfs(head);
+    }
+
+    ListNode h = null;
+    private boolean dfs(ListNode head) {
+        if(head == null) return true;
+        if(!dfs(head.next)) return false;
+        if(head.val != h.val) return false;
+        h = h.next;
+        return true;
+    }
+}
