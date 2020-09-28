25b66ab889be1b9928d2400ce1b9ccdcbbc4bcf7
YuanLicc <yl123456789lxp@163.com>
2020-09-28 23:18:58 +0800

    leetcode 322 dp dfs

--- algorithm/src/main/java/com/yl/learn/algorithm/bytedance/CoinChange.java
+++ algorithm/src/main/java/com/yl/learn/algorithm/bytedance/CoinChange.java
@@ -1,5 +1,9 @@
 package com.yl.learn.algorithm.bytedance;
 
+import junit.framework.TestCase;
+
+import java.util.Arrays;
+
 /**
  * 给定不同面额的硬币 coins 和一个总金额 amount。
  * 编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
@@ -15,22 +19,36 @@ package com.yl.learn.algorithm.bytedance;
  * 链接：https://leetcode-cn.com/problems/coin-change
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
-public class CoinChange {
+public class CoinChange extends TestCase {
 
-    int min = Integer.MAX_VALUE;
     public int coinChange(int[] coins, int amount) {
-        dfs(coins, amount, 0, 0);
+        Arrays.sort(coins);
+        int min = dfs(coins, amount, new int[amount]);
+        return min == Integer.MAX_VALUE ? -1 : min;
+    }
+
+    // min[amount] = min(amount - coins) + 1
+    private int dfs(int[] coins, int amount, int[] cache) {
+        if(amount == 0) return 0;
+        if(amount < 0) return Integer.MAX_VALUE;
+        if(cache[amount - 1] != 0) return cache[amount - 1];
+
+        int min = Integer.MAX_VALUE;
+        for(int i = coins.length - 1; i >= 0; i--) {
+            if(coins[i] <= amount) {
+                int x = dfs(coins, amount - coins[i], cache);
+                if(x != Integer.MAX_VALUE) {
+                    x = x + 1;
+                    min = min < x ? min : x;
+                }
+            }
+        }
+        cache[amount - 1] = min;
         return min;
     }
-    private void dfs(int[] coins, int amount, int sum, int cnt) {
-        if(amount > sum) return;
-        if(sum == amount) {
-            min = min < cnt ? min :cnt;
-            return;
-        }
-        for(int i = 0; i < coins.length; i++) {
-            dfs(coins, amount, sum + coins[i], cnt + 1);
-        }
+
+    public void test() {
+        coinChange(new int[]{1, 2, 5}, 11);
     }
 
 
