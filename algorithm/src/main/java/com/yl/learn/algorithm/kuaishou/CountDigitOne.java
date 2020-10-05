9b23b86f5338705c222534ba9787f17822ae1e5d
YuanLicc <yl123456789lxp@163.com>
2020-10-05 18:26:27 +0800

    leetcode 236

---
+++ algorithm/src/main/java/com/yl/learn/algorithm/kuaishou/CountDigitOne.java
@@ -0,0 +1,22 @@
+package com.yl.learn.algorithm.kuaishou;
+
+import com.yl.learn.util.util.PrintUtil;
+import junit.framework.TestCase;
+
+public class CountDigitOne extends TestCase {
+
+    public int countDigitOne(int n) {
+        int cnt = 0;
+        while (n > 0) {
+
+        }
+        return 0;
+    }
+
+    public void test() {
+        PrintUtil.println(10001 ^ 0);
+        PrintUtil.println(1 ^ 0);
+        PrintUtil.println(100 ^ 1001);
+    }
+
+}
