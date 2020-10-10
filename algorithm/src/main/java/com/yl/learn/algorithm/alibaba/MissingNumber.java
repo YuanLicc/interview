c9c3c693468fdfdbdbc8820c41b4d5be55f79d45
yuanlicc <yl123456789lxp@163.com>
2020-10-10 17:19:03 +0800

    leetcode 268 map 实现

---
+++ algorithm/src/main/java/com/yl/learn/algorithm/alibaba/MissingNumber.java
@@ -0,0 +1,33 @@
+package com.yl.learn.algorithm.alibaba;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个包含 0, 1, 2, ..., n 中 n 个数的序列，找出 0 .. n 中没有出现在序列中的那个数。
+ * 示例 1:
+ * 输入: [3,0,1]
+ * 输出: 2
+ * 示例 2:
+ * 输入: [9,6,4,2,3,5,7,0,1]
+ * 输出: 8
+ *
+ * 说明:
+ * 你的算法应具有线性时间复杂度。你能否仅使用额外常数空间来实现?
+ *
+ * 来源：力扣（LeetCode）268
+ * 链接：https://leetcode-cn.com/problems/missing-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MissingNumber {
+    
+    public int missingNumber(int[] nums) {
+        Map<Integer, Boolean> map = new HashMap<>(nums.length);
+        for(int i = 0; i < nums.length; i++) map.put(nums[i], false);
+        for(int i = 0; i <= nums.length; i++) {
+            if(map.get(i) == null) return i;
+        }
+        return -1;
+    }
+    
+}
