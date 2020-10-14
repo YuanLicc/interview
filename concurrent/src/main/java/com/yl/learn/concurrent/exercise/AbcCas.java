90050959b3767504f9b56c7dc01ed556c374ac8f
YuanLicc <yl123456789lxp@163.com>
2020-10-14 23:29:54 +0800

    leetcode con abc

---
+++ concurrent/src/main/java/com/yl/learn/concurrent/exercise/AbcCas.java
@@ -0,0 +1,44 @@
+package com.yl.learn.concurrent.exercise;
+
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
+public class AbcCas {
+    private static final Unsafe unsafe;
+
+    private final static long stateOffset;
+
+    static {
+        try {
+            Field field = Unsafe.class.getDeclaredField("theUnsafe");
+            field.setAccessible(true);
+            unsafe = (Unsafe) field.get(null);
+            stateOffset = unsafe.objectFieldOffset
+                    (AbcCas.class.getDeclaredField("state"));
+        } catch (Exception ex) { throw new Error(ex); }
+    }
+
+    private volatile int state = 0;
+
+    public void first() {
+        while(!unsafe.compareAndSwapInt(this, stateOffset, 0, 1)) {
+            //sleep
+        }
+        System.out.println(1);
+    }
+
+    public void second() {
+        while(!unsafe.compareAndSwapInt(this, stateOffset, 1, 2)) {
+            //sleep
+        }
+        System.out.println(2);
+    }
+
+    public void third() {
+        while(!unsafe.compareAndSwapInt(this, stateOffset, 2, 0)) {
+            //sleep
+        }
+        System.out.println(3);
+    }
+}
