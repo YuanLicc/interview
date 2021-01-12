25b043351b12582ef0ea33ffa60b0ed2f483f3c9
yuanlicc <yl123456789lxp@163.com>
2021-01-12 10:22:33 +0800

    15

---
+++ algorithm/src/main/java/com/yl/learn/algorithm/r20210111/OneFive.java
@@ -0,0 +1,62 @@
+package com.yl.learn.algorithm.r20210111;
+
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
+ * 注意：答案中不可以包含重复的三元组。
+ *
+ * 示例 1：
+ * 输入：nums = [-1,0,1,2,-1,-4]
+ * 输出：[[-1,-1,2],[-1,0,1]]
+ *
+ * 示例 2：
+ * 输入：nums = []
+ * 输出：[]
+ *
+ *
+ * 示例 3：
+ * 输入：nums = [0]
+ * 输出：[]
+ 
+ * 提示：
+ *
+ *  0 <= nums.length <= 3000
+ *  -105 <= nums[i] <= 105
+ *
+ *  Related Topics 数组 双指针
+ */
+public class OneFive extends TestCase {
+    
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        
+        List<List<Integer>> rs = new ArrayList<>();
+        
+        for (int i = 0; i < nums.length - 2; ) {
+            int left = i + 1;
+            int right = nums.length - 1;
+            
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                if(sum == 0) {
+                    rs.add(Arrays.asList(nums[i], nums[left++], nums[right--]));
+                    while (left < nums.length - 1 && nums[left] == nums[left - 1]) left++;
+                    while (right > i + 1 && nums[right] == nums[right + 1]) right--;
+                }
+                else if(sum > 0) right--;
+                else left++;
+            }
+            i++;
+            while (i < nums.length - 2 && nums[i] == nums[i - 1]) i++;
+        }
+        
+        return rs;
+    }
+}
+
