1b5a1beda34d1c7aa230d38cba3159fa6d4e32fc
yuanlicc <yl123456789lxp@163.com>
2021-03-04 15:51:21 +0800

    local

--- concurrent/src/test/java/com/yl/learn/concurrent/ThreadLocalTest.java
+++ concurrent/src/test/java/com/yl/learn/concurrent/ThreadLocalTest.java
@@ -1,16 +1,41 @@
 package com.yl.learn.concurrent;
 
-import com.yl.learn.concurrent.util.ThreadUtils;
 import com.yl.learn.util.util.PrintUtil;
 import junit.framework.TestCase;
+
+import java.lang.reflect.Field;
 
 public class ThreadLocalTest extends TestCase {
 
     public void testThreadLocal() {
-        Timer timer = Timer.start();
-
-        ThreadUtils.sleepSecond(1);
-
-        PrintUtil.println(timer.end());
+        
+        new OwnThread().start();
+        
     }
+    
+    class OwnThread extends Thread {
+        
+        public void run() {
+    
+            ThreadLocal<String> user = new ThreadLocal<>();
+            user.set("user1");
+    
+            ThreadLocal<String> name = new ThreadLocal<>();
+            name.set("name1");
+            
+            Field field = null;
+            try {
+                field = Thread.class.getDeclaredField("threadLocals");
+                field.setAccessible(true);
+                Object object = field.get(this);
+                PrintUtil.print(object);
+            } catch (NoSuchFieldException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+    
+        }
+        
+    }
+    
+    
 }
